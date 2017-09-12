e8edf7cb6fb3b780e453b87ad72d32abcf86d9df
YJsh <syj2014mail@163.com>
2017-09-12 11:44:29 +0800

    初始化导航栏

--- src/js/nav-sidebar.js
+++ src/js/nav-sidebar.js
@@ -1,12 +1,18 @@
 (function($) {
-    $.fn.navSidebar = function(){
+    $.fn.navSidebar = function(data){
         $("body").attr({
             "data-spy": "scroll",
             "data-target": ".scrollspy"
         });
-
-        return this.each(function(){
-
+        console.log(this);
+        this.empty();
+        this.append("<ul class='nav affix' data-spy='affix'></ul>");
+        u = this.children("ul");
+        $.each(data, function(index){
+            subData = data[index]
+            u.append("<li><a href='#" + subData["id"] + "'>" + subData["text"] + "</a></li>")
         });
+        u.children("li:first").attr("class", "active");
+        return this
     }
 })(jQuery)
